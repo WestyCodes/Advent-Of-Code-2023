c8a2004e318e48d7541b7c487e347c1de488372a
Oliver West <oliversimon.west@gmail.com>
2023-12-02 16:10:36 +0000

    changed all digitsFromString to nums

--- day1/dayOne.js
+++ day1/dayOne.js
@@ -12,19 +12,67 @@ function readFile(filePath) {
 
 const adventOfCodeInput = readFile('./day1/input.txt');
 // END OF GET FILE INPUT
+// PART ONE
 const inputArray = adventOfCodeInput.split('\r\n');
 
-function getDigits(string) {
-    const digitsFromString = string.match(/\d/g);
-    return Number(
-        digitsFromString[0] + digitsFromString[digitsFromString.length - 1]
-    );
+function getDigits(line) {
+    const nums = line.match(/\d/g);
+    return Number(nums[0] + nums[nums.length - 1]);
+}
+
+// PART TWO
+
+function getAllDigits(line) {
+    const numMap = {
+        one: '1',
+        two: '2',
+        three: '3',
+        four: '4',
+        five: '5',
+        six: '6',
+        seven: '7',
+        eight: '8',
+        nine: '9',
+    };
+    const nums = line
+        .replace('oneight', 'oneeight')
+        .replace('twone', 'twoone')
+        .replace('threeight', 'threeeight')
+        .replace('fiveight', 'fiveeight')
+        .replace('sevenine', 'sevennine')
+        .replace('nineight', 'nineeight')
+        .replace('eighthree', 'eightthree')
+        .replace('eightwo', 'eighttwo')
+        .match(
+            /(?:one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)/g
+        );
+
+    let num = nums[0].length > 1 ? numMap[nums[0]] : nums[0];
+
+    if (nums.length === 1) {
+        num += num;
+    }
+
+    if (nums.length > 1) {
+        num +=
+            nums[nums.length - 1].length > 1
+                ? numMap[nums[nums.length - 1]]
+                : nums[nums.length - 1];
+    }
+
+    return Number(num);
 }
 
 let total = 0;
 
+// Part One Total Finder
+// inputArray.forEach((element) => {
+//     total += getDigits(element);
+// });
+
+// Part Two Total Finder
 inputArray.forEach((element) => {
-    total += getDigits(element);
+    total += getAllDigits(element);
 });
 
 console.log(total);
